8edbf4fd3c62b99e9eba75d50031c66c59598392
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-12-31 12:06:13 +0100

    Added ESLint

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Home page.
+ *
+ * @returns {React.Element} The rendered element.
+ */
+const Index = () => (
+  <div>
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
