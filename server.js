45c30c5125f38484f8e79bfe4ac3f694a49f4bb2
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-06 21:47:50 +0100

    Added custom server with /i/:id route

---
+++ server.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const Koa = require('koa');
+const next = require('next');
+const Router = require('koa-router');
+
+/**
+ * Serve Next application with custom routes.
+ *
+ * @param {Object} config - Server settings,.
+ * @param {boolean} config.dev - Next serve development mode enabled or not.
+ * @param {number} config.port - Server port.
+ * @param {Object} config.routes - Paths to pages indexed by routes. Ex: `{ '/p/:id': /post }`.
+ * @returns {number} The port the server is listening on.
+ */
+async function serve({ dev, port, routes }) {
+  const app = next({ dev });
+  const handle = app.getRequestHandler();
+
+  await app.prepare();
+
+  const server = new Koa();
+  const router = new Router();
+
+  Object.entries(routes).forEach(([route, path]) => {
+    router.get(route, async ctx => {
+      await app.render(ctx.req, ctx.res, path, ctx.params);
+      ctx.respond = false;
+    });
+  });
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, nextStep) => {
+    ctx.res.statusCode = 200;
+    await nextStep();
+  });
+
+  server.use(router.routes());
+  await new Promise(resolve => server.listen(port, resolve));
+
+  return port;
+}
+
+// Start the server now
+/* eslint-disable no-console */
+// eslint-disable-next-line more/no-then
+serve({
+  dev: process.env.NODE_ENV !== 'production',
+  port: parseInt(process.env.PORT, 10) || 3000,
+  routes: {
+    '/i/:id': '/item',
+  },
+}).then(
+  actualPort => {
+    console.log(`> Ready on http://localhost:${actualPort}`);
+  },
+  error => {
+    console.error(error);
+    process.exit(1);
+  },
+);
+/* eslint-enable no-console */
