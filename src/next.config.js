5e7b1d85b8f7ea6eade70c93e91591db2af910be
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-06 09:37:24 +0100

    Moved {pages => src/pages} directory

---
+++ src/next.config.js
@@ -0,0 +1,9 @@
+/**
+ * Next.js configuration.
+ *
+ * @see Custom configuration documentation: https://github.com/zeit/next.js#custom-configuration
+ * @returns {Object} A plain object.
+ */
+module.exports = () => ({
+  distDir: '../.next',
+});
