6cf1e0c5d25f9ebb4c66f2382e8ced343d39f958
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-07 05:22:09 +0100

    Added Header and Layout components

---
+++ components/Layout.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import styled from 'styled-components';
+import ConnectedHeader from './Header';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 64rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 2rem;
+`;
+
+/**
+ * App layout.
+ *
+ * @param {*} children - Child components.
+ * @returns {React.Element} The rendered element.
+ */
+const Layout = ({ children }) => (
+  <>
+    <CssBaseline />
+    <ConnectedHeader />
+    <Container>{children}</Container>
+  </>
+);
+
+export default Layout;
